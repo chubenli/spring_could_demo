4169ec9c99cd32cd4ec0f1d5bfdb0f28b8017d64
lizhibin <do1lizhibin>
2018-09-21 14:31:47 +0800

    初始化demo

---
+++ eureka_service1/src/main/java/com/example/eureka_client/controller/ClientCtrl.java
@@ -0,0 +1,17 @@
+package com.example.eureka_client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientCtrl {
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "test") String name) {
+        return "你好, " + name + " ,我是服务1，我的端口号是:" + port;
+    }
+}
