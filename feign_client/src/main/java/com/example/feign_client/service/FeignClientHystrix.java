533caf5238975ace1dcc152a0af762ad16a5f511
lizhibin <do1lizhibin>
2018-10-10 11:47:04 +0800

    springCould升级到F版

--- feign_client/src/main/java/com/example/feign_client/service/FeignClientHystrix.java
+++ feign_client/src/main/java/com/example/feign_client/service/FeignClientHystrix.java
@@ -1,13 +1,16 @@
 package com.example.feign_client.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
 public class FeignClientHystrix implements IFeignClientService  {
-
+    private Logger logger = LoggerFactory.getLogger(IFeignClientService.class);
     @Override
     public Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
+        logger.info("feign断路器，服务不可用。。。");
         return -999999;
     }
 }
--- ribbon_client/src/main/java/com/example/ribbon_client/service/RibbonComputeService.java
+++ ribbon_client/src/main/java/com/example/ribbon_client/service/RibbonComputeService.java
@@ -1,6 +1,8 @@
 package com.example.ribbon_client.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,8 @@ import java.util.Map;
 
 @Service
 public class RibbonComputeService {
-
     private final RestTemplate restTemplate;
-
+    private Logger logger = LoggerFactory.getLogger(RibbonComputeService.class);
     @Autowired
     public RibbonComputeService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
@@ -29,6 +30,7 @@ public class RibbonComputeService {
     }
 
     public ResponseEntity<Integer> addServiceFallback(Integer a, Integer b) {
+        logger.info("ribbon断路器，服务不可用。。。");
         return new ResponseEntity<>((Integer)null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
