e8de20ded0bc0f67074488ece7fffafadb1fd017
chibun <chibun_li@163.com>
2018-12-23 17:54:46 +0800

    spring eureka 注册中心增加登录认证

---
+++ eureka_server/src/main/java/com/example/eureka_server/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.eureka_server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+    /**
+     * 注册中心开启登录认证后，为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,如果是form方式,不能使用url格式登录
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable(); //关闭csrf
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); //开启认证
+
+
+    }
+
+}
--- zipkin-server/src/main/java/com/example/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/example/zipkinserver/ZipkinServerApplication.java
@@ -3,16 +3,19 @@ package com.example.zipkinserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
-@EnableZipkinServer
+@EnableZipkinStreamServer
+//使用MQ做服务发现
+//@EnableZipkinServer
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ZipkinServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
 
 }
 
